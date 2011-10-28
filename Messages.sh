2df593151913cdfd5b1dcb01da7f667abbed29ba
Aleix Pol <aleixpol@kde.org>
2011-10-28 02:24:51 +0200

    Adapt the Messages.sh file

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$XGETTEXT src/*.cpp analitzagui/*.cpp analitza/*.cpp -o $podir/kalgebra.pot
+$XGETTEXT analitzagui/*.cpp analitza/*.cpp -o $podir/analitza.pot
 $XGETTEXT plasmoid/*.cpp -o $podir/plasma_applet_kalgebra.pot
 
