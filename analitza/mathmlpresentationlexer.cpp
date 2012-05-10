5bd567b9f1a778af7e56c7b5739e4d485ece1d1a
Aleix Pol <aleixpol@kde.org>
2012-05-10 04:32:35 +0200

    slightly improve mathml presentation tokenization

--- analitza/mathmlpresentationlexer.cpp
+++ analitza/mathmlpresentationlexer.cpp
@@ -84,16 +84,21 @@ void MathMLPresentationLexer::getToken()
 						m_tokens.takeLast();
 					
 					int t;
-					QString op=m_xml.text().toString();
+					QString op=m_xml.text().toString().trimmed();
 					if(op.length()==1 && m_operators.contains(op[0]))
 						t = m_operators[op[0]];
 					else if(op.length()==2 && m_longOperators.contains(op))
 						t = m_longOperators[op];
 					else
-						m_err= i18nc("Error message", "Unknown token %1", op);
+						m_err= i18nc("Error message", "Unknown token '%1'", op);
 					m_tokens.append(TOKEN(t, 0));
-				} else
-					qDebug() << "dunno" << m_xml.text().toString();
+				} else {
+					QString text = m_xml.text().toString().trimmed();
+					if(text=="if" || text=="otherwise") {
+						m_tokens.append(TOKEN(ExpressionTable::tQm, 0, m_xml.text().toString()));
+					} else
+						qDebug() << "dunno" << text;
+				}
 				break;
 		}
 	}
