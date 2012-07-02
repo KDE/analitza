9fa7018e1a734b1da15bcff8bd7e6971982d1fe6
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2012-07-02 13:59:51 -0500

    fix/update credits in some files

--- analitzaplot/plotview3d.h
+++ analitzaplot/plotview3d.h
@@ -1,5 +1,5 @@
 /*************************************************************************************
- *  Copyright (C) 2010 by Percy Camilo T. Aucahuasi <percy.camilo.ta@gmail.com>      *
+ *  Copyright (C) 2010-2012 by Percy Camilo T. Aucahuasi <percy.camilo.ta@gmail.com> *
  *                                                                                   *
  *  This program is free software; you can redistribute it and/or                    *
  *  modify it under the terms of the GNU General Public License                      *
