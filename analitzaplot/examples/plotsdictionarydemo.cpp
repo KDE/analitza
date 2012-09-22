01aee73ba466387e50f9888ec3f8eeac1c04bee7
Aleix Pol <aleixpol@kde.org>
2012-09-22 23:28:50 +0200

    Create another analitzaplot example
    This time the DictionaryModel

---
+++ analitzaplot/examples/plotsdictionarydemo.cpp
@@ -0,0 +1,54 @@
+/*************************************************************************************
+ *  Copyright (C) 2012 by Aleix Pol Gonzalez <aleixpol@kde.org>                      *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include <QMainWindow>
+#include <QTreeView>
+#include <QVBoxLayout>
+
+#include <kapplication.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <plotsdictionariesmanager.h>
+
+int main(int argc, char *argv[])
+{
+    KAboutData aboutData("PlotsDictionaryDemo",
+                         0,
+                         ki18n("PlotsDictionaryDemo"),
+                         "1.0",
+                         ki18n("PlotsDictionaryDemo"),
+                         KAboutData::License_LGPL_V3,
+                         ki18n("(c) 2012 Aleix Pol Gonazlez"),
+                         ki18n("PlotsDictionaryDemo"),
+                         "http://www.kde.org");
+
+    KCmdLineArgs::init(argc, argv, &aboutData);
+    KApplication app;
+
+    QWidget central;
+    central.resize(800,600);
+    QHBoxLayout *layout = new QHBoxLayout(&central);
+    QTreeView* tree = new QTreeView(&central);
+    PlotsDictionariesManager m;
+    tree->setModel(m.model());
+    layout->addWidget(tree);
+    
+    central.show();
+    return app.exec();
+}
+
