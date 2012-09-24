4f25e3c383d3c1c84c8dabc0a6df96f79e862bbe
Albert Astals Cid <aacid@kde.org>
2012-09-24 22:01:22 +0200

    Fix the MEssage.sh

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-sed 's/.*\/\/\([a-z ]*\)\/\/.*/i18n(\"dictionary\", \"\1\")/' analitzaplot/data/plots/*.plots | grep i18nc\(\" > dictionaryplots_i18n.cpp
+sed 's/.*\/\/\([a-z ]*\)\/\/.*/i18nc(\"dictionary\", \"\1\");/' analitzaplot/data/plots/*.plots | grep i18nc\(\" > dictionaryplots_i18n.cpp
 $XGETTEXT analitzagui/*.cpp analitza/*.cpp dictionaryplots_i18n.cpp -o $podir/analitza.pot
 
 rm -f dictionaryplots_i18n.cpp
