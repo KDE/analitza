fd740918fb3f071be184df42d10f638ea328a720
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2013-03-13 00:29:00 -0500

    Fix build, add shapedata_p and remove old concepts

---
+++ analitzaplot/shapedata_p.h
@@ -0,0 +1,42 @@
+/*************************************************************************************
+ *  Copyright (C) 2013 by Percy Camilo T. Aucahuasi <percy.camilo.ta@gmail.com>      *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#ifndef ANALITZAPLOT_PLOTITEM_H
+#define ANALITZAPLOT_PLOTITEM_H
+#include <QColor>
+#include "plottingenums.h"
+#include <analitza/expression.h>
+
+namespace Analitza {
+    
+struct ShapeData
+{
+    QColor m_color;
+    CoordinateSystem m_coordinateSystem;
+    Dimension m_dimension;
+    QStringList m_errors;
+    Expression m_expression; //visible expression
+    QString m_iconName;
+    QString m_name;
+    QString m_typeName;
+    bool m_visible;
+};
+
+}
+
+#endif // ANALITZAPLOT_PLOTITEM_H
