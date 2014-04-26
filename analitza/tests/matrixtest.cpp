1fd095d05201b5909d7432ed061ad37d68dfdaaf
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2014-04-25 22:05:39 -0500

    Setup basic infrastructure for matrix builtin methods.

---
+++ analitza/tests/matrixtest.cpp
@@ -0,0 +1,116 @@
+/*************************************************************************************
+ *  Copyright (C) 2014 by Percy Camilo T. Aucahuasi <percy.camilo.ta@gmail.com>      *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include "matrixtest.h"
+
+#include <QtTest/QTest>
+
+#include "analyzer.h"
+//#include <operations.h>
+
+using Analitza::Expression;
+
+QTEST_MAIN( MatrixTest )
+
+MatrixTest::MatrixTest(QObject *parent)
+ : QObject(parent)
+{}
+
+MatrixTest::~MatrixTest()
+{}
+
+void MatrixTest::initTestCase()
+{
+	a=new Analitza::Analyzer;
+}
+
+void MatrixTest::cleanupTestCase()
+{
+	delete a;
+}
+
+void MatrixTest::testBuiltinMethods_data()
+{
+	//add matrix strings in analitza language
+// 	QTest::addColumn<QString>("expression");
+// 	QTest::addColumn<double>("result");
+// 
+// 	QTest::newRow("a value") << "2" << 2.;
+// 	QTest::newRow("val.e0") << "12.0e-02" << 12e-2;
+// 	QTest::newRow("vale") << "12e-2" << 12e-2;
+// 	QTest::newRow("val") << "12e2" << 12e2;
+// 	
+// 	QTest::newRow("simple addition") << "2+2" << 4.;
+// 	QTest::newRow("simple power") << "2**99" << pow(2., 99.);
+// 	QTest::newRow("simple multiplication") << "3*3" << 9.;
+// 	QTest::newRow("sinus") << "sin(3*3)" << sin(9.);
+// 	QTest::newRow("declare") << "x:=3" << 3.;
+// 	QTest::newRow("sum") << "sum(x : x=1..99)" << 4950.;
+// 	QTest::newRow("diff") << "(diff(x:x))(1)" << 1.;
+// 	QTest::newRow("diffz") <<"(diff(z:z))(1)" << 1.;
+// 	
+// 	QTest::newRow("product") << "product(n : n=1..5)" << 120.;
+// 	QTest::newRow("factorial") << "factorial(5)" << 120.;
+// 	
+// 	QTest::newRow("simple piecewise") << "piecewise { pi=0? 3, pi=pi?33 }" << 33.;
+// 	QTest::newRow("simple piecewise with otherwise") << "piecewise { pi=0? 3, ?33 }" << 33.;
+// 	QTest::newRow("boolean and") << "and(true,false)" << 0.;
+// 	QTest::newRow("boolean or") << "or(false,true)" << 1.;
+// 	QTest::newRow("boolean not") << "not(false)" << 1.;
+// 	QTest::newRow("lambda")  << "(x->x+2)(2)" << 4.;
+// 	QTest::newRow("lambda2") << "(x->3*x^2)(1)" << 3.;
+// 	QTest::newRow("lambda3") << "(x->x*sum(t:t=0..3))(2)" << 12.;
+// 	QTest::newRow("imaginarypow") << "(-4)^(1/4)" << 1.;
+// 	QTest::newRow("imaginaryroot") << "root(-4, 4)" << 1.;
+// 
+// 	//comprehension
+// 	QTest::newRow("sum.2bvars") << "sum(x*y : (x, y)=1..3)" << 36.;
+// 	QTest::newRow("sum.list") << "sum(x : x@list{1,5,44})" << 50.;
+// 
+// 	QTest::newRow("sum.sum") << "sum(sum(x : x=0..i) : i=0..10)" << 220.;
+// 
+// 	QTest::newRow("exists") << "exists(x : x@list{true,true,false})" << 1.;
+// 	QTest::newRow("forall") << "forall(x : x@list{true,true,false})" << 0.;
+// // 	QTest::newRow("emptysum") << "sum(x : x@list{})" << 0.;
+// 	
+// 	QTest::newRow("lambdacall") << "f:=x->f(x)" << 0.;
+}
+
+void MatrixTest::testBuiltinMethods()
+{
+	//get hos matrix strinx invalid Analitza lang and test commands
+// 	QFETCH(QString, expression);
+// 	QFETCH(double, result);
+// 	Expression e(expression, false);
+// 	if(!e.isCorrect()) qDebug() << "error: " << e.error();
+// 	QCOMPARE(e.isCorrect(), true);
+// 	
+// 	a->setExpression(e);
+// 	
+// 	if(!a->isCorrect()) qDebug() << "error: " << a->errors();
+// 	QVERIFY(a->isCorrect());
+// 	QCOMPARE(a->evaluate().toReal().value(), result);
+// 	QVERIFY(a->isCorrect());
+// 	Expression ee=a->calculate();
+// 	if(!a->isCorrect()) qDebug() << "error: " << a->errors();
+// 	QVERIFY(a->isCorrect());
+// 	QCOMPARE(ee.toReal().value(), result);
+// 	QVERIFY(a->isCorrect());
+}
+
+#include "matrixtest.moc"
