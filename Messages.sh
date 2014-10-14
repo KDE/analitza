a284a66cf3ec4d8d56c7597f9a7baed65513030e
Aleix Pol <aleixpol@kde.org>
2014-10-15 00:30:07 +0200

    Iterate Messages.sh
    Don't remove rc.cpp, l10n scripts will do it. Also call the file _qt,
    because it will use Qt translation subsystem.

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
 perl extractplots --context="dictionary" analitzaplot/data/plots/*.plots >> rc.cpp
-$XGETTEXT analitzagui/*.cpp analitza/*.cpp rc.cpp -o $podir/analitza.pot
-
-rm -f rc.cpp
+$XGETTEXT_QT analitzaplot/*.cpp analitzagui/*.cpp analitza/*.cpp rc.cpp -o $podir/analitza_qt.pot
