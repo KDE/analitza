ac4db32ca36cb269055942d195fda9bed7c458de
Aleix Pol <aleixpol@kde.org>
2014-10-16 03:05:05 +0200

    Ensure we are generating the translation files properly
    Does the cmake magic, both to generate the file and load it.
    Uses the proper executable to extact the tr() strings
    
    REVIEW: 120591

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 perl extractplots --context="dictionary" analitzaplot/data/plots/*.plots >> rc.cpp
-$XGETTEXT_QT analitzaplot/*.cpp analitzagui/*.cpp analitza/*.cpp rc.cpp -o $podir/analitza_qt.pot
+$EXTRACT_TR_STRINGS analitzaplot/*.cpp analitzagui/*.cpp analitza/*.cpp rc.cpp -o $podir/analitza_qt.pot
