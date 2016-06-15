7a505c13d6f9dca128e5545ca81239c3933242c4
Aleix Pol <aleixpol@kde.org>
2016-06-15 16:18:26 +0200

    Qt 5.7 outputs floats differently
    BUG: 364346

--- analitza/tests/analitzatest.cpp
+++ analitza/tests/analitzatest.cpp
@@ -448,7 +448,11 @@ void AnalitzaTest::testCorrection_data()
            << QStringLiteral("pu:=n->sum(  comb(n,i)*p^(n-i)*(1-p)*sum(x:x=0..i)  :i=0..(floor((n-1)/2)))")
            << QStringLiteral("pu(5)");
     
+#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+    QTest::newRow("bug241047") << script << "2.97495e-5";
+#else
     QTest::newRow("bug241047") << script << "2.97495e-05";
+#endif
     
     script.clear();
     script << QStringLiteral("comb:=(n, i)->factorial(n)/(factorial(i)*factorial(n-i))")
